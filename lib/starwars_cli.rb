83ee754654e0e208ddadb9558b6108f4ffec2a87
guerino antonini <guerantonini94@gmail.com>
2020-05-26 21:08:33 -0400

    WIP: Adding initial file/folder structure

---
+++ lib/starwars_cli.rb
@@ -0,0 +1,11 @@
+
+#app enviroment
+require 'pry'
+require 'rest-client'
+require 'json'
+
+
+
+require_relative './starwars_cli/cli.rb'
+require_relative './starwars_cli/api.rb'
+require_relative './starwars_cli/starwars.rb'
---
+++ lib/starwars_cli/starwars.rb
@@ -0,0 +1,3 @@
+class Starwars
+    puts "starwars class loaded"
+end
